2224cc6823fece99e2e8c30d7e8431af66fabb05
x2004style <frostmax2016@gmail.com>
2025-07-18 00:39:33 +0300

    added assign roles view

--- src/app/admin/users-table/users-table.component.ts
+++ src/app/admin/users-table/users-table.component.ts
@@ -7,7 +7,9 @@ interface User {
   email: string;
   roles: string[];
   emailAvailable: boolean;
-  selected: boolean;
+  selected?: boolean;
+  markedForDeletion?: boolean;
+  result?: string;
 }
 
 @Component({
@@ -92,13 +94,11 @@ export class UsersTableComponent {
     });
   }
 
-  assignRolesTest() {
-    console.log('assignroles');
-  }
 
   filterTest() {
     console.log('filter')
   }
+
   allRoles: string[] = ['Admin', 'Operator', 'User', 'Manager', 'Analyst', 'Guest'];
   selectedRoles: string[] = [];
 
@@ -107,28 +107,45 @@ export class UsersTableComponent {
   toggleRoleAssignment() {
     if (this.hasSelectedUsers) {
       this.showRoleAssignment = !this.showRoleAssignment;
-      this.selectedRoles = this.getSelectedUsersRoles();
     }
-  }
-
-  getSelectedUsersRoles(): string[] {
-    const roles = new Set<string>();
-    this.users
-      .filter(user => user.selected)
-      .forEach(user => user.roles.forEach(role => roles.add(role)));
-    return Array.from(roles);
   }
 
   isRoleSelected(role: string): boolean {
     return this.selectedRoles.includes(role);
   }
 
-  applyRoles() {
-    this.users
-      .filter(user => user.selected)
-      .forEach(user => {
-        user.roles = [...this.selectedRoles];
-      });
+  closeSidenav() {
     this.showRoleAssignment = false;
+  }
+
+  showNewView = false;
+
+  cancelProcess(): void {
+    this.showNewView = false;
+    this.users.forEach(u => u.selected = false);
+  }
+
+  runProcess(): void {
+    this.users.forEach(u => u.result = '-');
+    const selected = this.users.filter(u => u.selected);
+    if (selected.length > 0) {
+      selected.slice(0, 2).forEach(u => u.result = 'Passed');
+      selected[2].result = 'Not passed';
+    }
+  }
+
+  toggleNewView(): void {
+    this.showNewView = !this.showNewView;
+    if (this.showNewView) {
+      this.users.forEach(u => u.markedForDeletion = false);
+    }
+  }
+
+  deleteSelected(): void {
+    this.users = this.users.filter(u => !u.markedForDeletion);
+  }
+
+  toggleDeleteSelection(user: User) {
+    user.markedForDeletion = !user.markedForDeletion;
   }
 }
